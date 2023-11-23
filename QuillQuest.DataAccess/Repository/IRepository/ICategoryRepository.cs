59c536f010d35c8e64a7e2d359e49d2209de1fed
coder om <mail.coderom@gmail.com>
2023-11-23 18:20:47 +0530

    Add project files.

---
+++ QuillQuest.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuillQuest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuillQuest.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}
