58dc60889e082e399ef2eb2e7c75dfb9eeeb1550
coder om <mail.coderom@gmail.com>
2023-11-30 23:55:19 +0530

    added temp.cs

---
+++ QuillQuest.DataAccess/Data/temp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuillQuest.DataAccess.Data
+{
+    public class Temp
+    {
+        /* This is a temp class created just to make continue the streak on github */
+    }
+}
